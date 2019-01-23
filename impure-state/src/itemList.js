c0b2e051265ab8e163e829348e8a89a4745f9292
Richardyoon91 <richardyoon91@gmail.com>
2019-01-23 16:16:15 -0600

    pushing local exercise to github

---
+++ impure-state/src/itemList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ItemList extends React.PureComponent {
+    static propTypes = {
+        items: PropTypes.array.isRequired
+    }
+
+
+    render () {
+        return (
+            <ul>
+                {this.props.items.map(item => 
+                    <li key = {item.id}>{item.value}</li>
+                )}
+            </ul>
+        );
+    }
+}
+
+export {ItemList};
